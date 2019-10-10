120541d7c53e2a98098af224788806ec3e71fb61
Minato1Namikaze <halo_10_dead@gmail.com.mx>
2019-10-10 13:04:32 -0500

    subiendo archivo

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let cantidadDeDolares = 500
+let tasaCeCambio = 19
+let dineroAAhorrar = cantidadDeDolares * tasaCeCambio
+console.log("la cantidad que debe ahorrar es:", cantidadDeDolares * tasaCeCambio)
+
+
+let horaDeDormir = 4
+
+let horaDeDespertar = 6
+
+let horasDormidas = horaDeDespertar - horaDeDormir
+
+console.log("Cantidad de horas dormidas:", horasDormidas)
+
+
+let cantidadDeAmigos = 5
+
+let costoAPagar = 4500
+
+let propina = costoAPagar * 10 / 100
+
+let consumoTotal = costoAPagar + propina
+
+let dividirEntreAmigos = consumoTotal/cantidadDeAmigos
+
+console.log("El dinero que tiene que dar cada uno es:",dividirEntreAmigos)
